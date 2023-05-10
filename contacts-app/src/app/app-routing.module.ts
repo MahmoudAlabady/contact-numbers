ffab6a49e89a7ecd25a6a0be54f2737fee4240fe
MahmoudAlabady <36644832+MahmoudAlabady@users.noreply.github.com>
2023-05-10 08:25:20 +0300

    Update app-routing.module.ts

--- contacts-app/src/app/app-routing.module.ts
+++ contacts-app/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { ContactListComponent } from './contact-list/contact-list.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
   {path:'contact-list',component:ContactListComponent},
   {path:'login',component:LoginComponent}
 ];
